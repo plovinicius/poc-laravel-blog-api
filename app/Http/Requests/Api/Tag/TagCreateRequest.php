a6340bc0ab489f5930e16aee301706ad2159bb8c
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-08-08 19:57:06 -0300

    feat(Requests): add TagCreateRequest for api

---
+++ app/Http/Requests/Api/Tag/TagCreateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Api\Tag;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TagCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:255', 'string'],
+            'slug' => ['required', 'max:255', 'string'],
+            'is_active' => ['required', 'bool']
+        ];
+    }
+}
