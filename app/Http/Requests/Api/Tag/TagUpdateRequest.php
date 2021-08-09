d213dd8bfb024c2e35b6eef98d6ce11216c2fc9a
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-08-08 23:20:37 -0300

    feat(requests): add TagUpdateRequest

---
+++ app/Http/Requests/Api/Tag/TagUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Api\Tag;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TagUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+{
+        return [
+            'name' => ['required', 'max:255', 'string'],
+            'slug' => ['required', 'max:255', 'string', 'unique:tags,slug,'. $this->id],
+            'is_active' => ['sometimes', 'boolean']
+        ];
+    }
+}
